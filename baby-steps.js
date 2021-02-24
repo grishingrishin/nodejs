553b15caf0e57dd4291fef9502beab9e90929f80
Егор <ge@devspark.ru>
2021-02-24 14:54:50 +0300

    Added new my-first-io.js file

---
+++ baby-steps.js
@@ -0,0 +1,14 @@
+// Declarative
+// const numbers = process.argv.slice(2);
+// if (!numbers.length) throw new Error("Numbers is not defined");
+// const sum = numbers.reduce((a, b) => +a + +b);
+
+// Imperative
+// const argv = process.argv;
+// let sum = 0;
+
+// for (let i = 2, len = argv.length; i < len; i++) {
+//   sum += Number(argv[i]);
+// }
+
+console.log(sum);
---
+++ my-first-io.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filePath = process.argv[2];
+
+// prettier-ignore
+console.log(fs.readFileSync(filePath, "utf-8").toString().split("\n").length - 1);
