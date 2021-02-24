b83143d5c2dcdb357303f9aeace141dbcbecafb0
Егор <ge@devspark.ru>
2021-02-24 15:21:00 +0300

    Completed my-first-async-io

---
+++ my-first-async-io.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.argv[2];
+
+fs.readFile(filePath, "utf-8", (err, data) => {
+  try {
+    const lines = data.split("\n").length - 1;
+    console.log(lines);
+  } catch (e) {
+    throw err;
+  }
+});
