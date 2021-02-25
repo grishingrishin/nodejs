a71d73992979490e6200e656a416174337a04d4c
Егор <ge@devspark.ru>
2021-02-25 15:48:31 +0300

    Completed a http-collect.js file

---
+++ http-collect.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const bl = require('bl');
+
+http.get(process.argv[2], res => {
+    const { statusCode } = res;
+
+    if (statusCode !== 200) {
+        const error = new Error(`Response failed \n Status code: ${statusCode}`);
+        console.error(error);
+        res.resume();
+        return;
+    }
+
+    res.setEncoding('utf-8');
+
+    res.pipe(
+        bl((err, data) => {
+            if (err) return err;
+            const length = data.length;
+            const str = data.toString();
+            return console.log(`${length}\n${str}`);
+        })
+    );
+});
