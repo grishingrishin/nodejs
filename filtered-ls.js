d9e89734f0f8c1ba9643104475c827a894b015c0
Егор <ge@devspark.ru>
2021-02-25 10:56:35 +0300

    Removed try catch

--- filtered-ls.js
+++ filtered-ls.js
@@ -4,11 +4,8 @@ const dirPath = process.argv[2];
 const filesExtensions = process.argv[3];
 
 fs.readdir(dirPath, (err, list) => {
-    try {
-        const filtered = list.filter(file => path.extname(file) == `.${filesExtensions}`);
-        const lines = filtered.join('\n');
-        console.log(lines);
-    } catch (e) {
-        throw err;
-    }
+    if (err) throw err;
+    const filtered = list.filter(file => path.extname(file) == `.${filesExtensions}`);
+    const lines = filtered.join('\n');
+    return console.log(lines);
 });
--- my-first-async-io.js
+++ my-first-async-io.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    try {
-        const lines = data.split('\n').length - 1;
-        console.log(lines);
-    } catch (e) {
-        throw err;
-    }
+    if (err) throw err;
+    const lines = data.split('\n').length - 1;
+    return console.log(lines);
 });
